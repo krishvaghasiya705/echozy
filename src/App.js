e6f08d34df4caf785dba1b18675f8b0b96701a56
krish vaghasiya <kvaghasiya705@gmail.com>
2025-03-28 11:43:15 +0530

    Update Google OAuth client ID in App component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 function App() {
 
   return (
-    <GoogleOAuthProvider clientId="888186814130-doeqguqj4t6lci7503jh9rscmsdtcjtl.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="888186814130-dc8h7bt7kvvgu6oou9gjm2vh7b4qr921.apps.googleusercontent.com">
       <RouterProvider router={router} />
     </GoogleOAuthProvider>
   );
